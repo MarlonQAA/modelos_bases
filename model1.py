6b1932851a125e1878b832ed2607d6723f59ed90
MarlonQAA <marlonquisa@gmail.com>
2026-03-25 04:21:10 +0000

    agregando model

---
+++ model1.py
@@ -0,0 +1,10 @@
+
+import torch.nn as nn
+
+class MiModelo(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc = nn.Linear(10, 2)
+
+    def forward(self, x):
+        return self.fc(x)
