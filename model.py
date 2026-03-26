4109aa182641e31c553be23891f88293c43ef079
MarlonQAA <marlonquisa@gmail.com>
2026-03-26 16:56:41 +0000

    agregando model

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 
+import torch
 import torch.nn as nn
 
 class CNNClassifier(nn.Module):
@@ -9,7 +10,7 @@ class CNNClassifier(nn.Module):
                n_clases = 10,
                kernel_size = 3
                ):
-    
+
     super(CNNClassifier,Self).__init__()
 
     L = []
@@ -23,7 +24,7 @@ class CNNClassifier(nn.Module):
               kernel_size=kernel_size,
               stride=1,
               padding=kernel_size
-          )    
+          )
       )
 
       L.append(nn.BatchNorm2d(num_features=n_l))
@@ -51,8 +52,34 @@ class CNNClassifier(nn.Module):
     self.classifier = nn.Sequential(*L2)
 
 
-def forward(self,x):
-  x = self.network(x)
-  x = x.mean(dim = [2,3])
-  x = self.classifier(x)
-  return x
+  def forward(self,x):
+    x = self.network(x)
+    x = x.mean(dim = [2,3])
+    x = self.classifier(x)
+    return x
+
+##### Guarda Pesos del modelo que se empieza a entrenar #####
+def save_model(model: nn.Module, name: str):
+    from torch import save
+    return save(model.state_dict(), name)
+
+#{
+#    "conv1.weight": tensor(...),
+#    "conv1.bias": tensor(...),
+#    "fc.weight": tensor(...),
+#    "fc.bias": tensor(...),
+#}
+
+##### Carga y mueve el modelo
+def load_model(name: str, device=None):
+    from torch import load
+    #Vemos si existe algun GPU disponible
+    if device is None:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    else:
+        device = torch.device(device)
+
+    r = CNNClassifier() #Esto construye un modelo nuevo, vacío, con pesos inicializados aleatoriamente.
+    r.load_state_dict(load(name, map_location=device, weights_only=True)) # cargas los pesos con load_state_dict() a r.
+    r.to(device)
+    return r
